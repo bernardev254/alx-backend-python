84c9d19700ef848020c45f091166d3cd709b2099
bkaraba <bkaraba14@gmail.com>
2022-04-16 08:53:05 +0000

    fix

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """module containing a function returning asyncio task"""
 import asyncio
+from typing import Type
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int):
+def task_wait_random(max_delay: int) -> Type[asyncio.Task]:
     """function returning asyncio task"""
     return asyncio.create_task(wait_random(max_delay))
