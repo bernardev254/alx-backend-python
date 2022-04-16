781484a4b387f2dae9c3da61f288226782260991
bkaraba <bkaraba14@gmail.com>
2022-04-16 06:22:59 +0000

    updates

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,6 +8,7 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """couroutine returning a list of returns"""
     task1 = asyncio.create_task(wait_random(max_delay))
